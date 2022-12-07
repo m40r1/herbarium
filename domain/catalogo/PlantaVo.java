dded7a00c4e26d0f97dfea738be0e6163764736b
m40r1 <99940252+m40r1@users.noreply.github.com>
2022-12-07 18:26:15 +0000

    Modelando Vos e jardim

---
+++ domain/catalogo/PlantaVo.java
@@ -0,0 +1,51 @@
+package org.site.herbarium.domain.catalogo;
+
+import org.site.herbarium.domain.generico.Estacao;
+import org.site.herbarium.generico.Bioma;
+
+public class PlantaVo {
+
+    private final String NomePopular;
+    /*
+     * TODO are there macros in java? if there are i can hava a macro to update a
+     * struct of biominal names with all the cataloged biominal names so that i can
+     * use it as a type and its a "easy" way to make ir adaptative
+     */
+    private final String NomeBinomial;
+    private final String EfeitosMedicinais;
+    private final String Indicacoes;
+    private final String ContraIndicacoes;
+    // Tempo medio para germinar
+    private Long germinar;
+    private Bioma bioma;
+    private Estacao estacao;
+
+    public PlantaVo(final String nomePopular, final String nomeBinomial, final String efeitosMedicinais,
+            final String indicacoes, final String contraIndicacoes) {
+        NomePopular = nomePopular;
+        NomeBinomial = nomeBinomial;
+        EfeitosMedicinais = efeitosMedicinais;
+        Indicacoes = indicacoes;
+        ContraIndicacoes = contraIndicacoes;
+    }
+
+    public String getNomePopular() {
+        return NomePopular;
+    }
+
+    public String getNomeBinomial() {
+        return NomeBinomial;
+    }
+
+    public String getEfeitosMedicinais() {
+        return EfeitosMedicinais;
+    }
+
+    public String getIndicacoes() {
+        return Indicacoes;
+    }
+
+    public String getContraIndicacoes() {
+        return ContraIndicacoes;
+    }
+}
---
+++ domain/generico/Clima.java
@@ -0,0 +1,14 @@
+package org.site.herbarium.generico;
+
+public enum Clima {
+    DESERTICO,
+    EQUATORIAL,
+    FrioDeMontanha,
+    MEDITERRANEO,
+    POLAR,
+    SEMIARIDO,
+    SUBPOLAR,
+    SUBTROPICAL,
+    TEMPERADO,
+    TROPICAL
+}
